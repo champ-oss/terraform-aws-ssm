1e25882fa96a999750c304cc33b0e9f05b26c6da
Justin Reeves <reeves122@gmail.com>
2024-06-12 13:04:15 -0500

    remove on_failure

--- cloudformation.tf
+++ cloudformation.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudformation_stack" "ram_permission" {
   count = length(var.shared_accounts) == 0 ? 0 : 1
   name  = "${var.git}-ssm-${random_string.identifier.result}"
-  on_failure = "DELETE"
+
   template_body = jsonencode({
     Resources = {
       RamPermission = {
