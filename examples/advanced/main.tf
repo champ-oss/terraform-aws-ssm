8dc224338d1b4e5bfca61bb52ff75cea41f4ae20
Justin Reeves <reeves122@gmail.com>
2024-06-12 13:15:47 -0500

    update test account id

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -1,12 +1,10 @@
 # Create an advanced encrypted SSM parameter
 module "this" {
-  source          = "../../"
-  git             = "terraform-aws-ssm"
-  name            = "/terraform-aws-ssm/advanced"
-  value           = "this is a test"
-  enable_kms      = true
-  shared_accounts       = ["123456789012"]
-  shared_principal_arns = [
-    "arn:aws:iam::123456789012:role/aws-reserved/sso.amazonaws.com/us-east-2/AWSReservedSSO_admin*"
-  ]
+  source                = "../../"
+  git                   = "terraform-aws-ssm"
+  name                  = "/terraform-aws-ssm/advanced"
+  value                 = "this is a test"
+  enable_kms            = true
+  shared_accounts       = []
+  shared_principal_arns = []
 }
