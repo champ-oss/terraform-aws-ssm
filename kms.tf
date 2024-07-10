24471d927db1bc0b9b39af8b85967f13e3ff9115
Phillip Langley <philliplangley@champmbplangley.lan>
2024-07-10 15:34:26 -0500

    test with enabled set false

--- kms.tf
+++ kms.tf
@@ -2,7 +2,7 @@ module "kms" {
   count                   = var.enable_kms ? 1 : 0
   source                  = "github.com/champ-oss/terraform-aws-kms.git?ref=v1.0.34-a5b529e"
   git                     = var.git
-  name                    = "alias/${var.git}-ssm-${random_string.identifier[0].result}"
+  name                    = var.enabled ? "alias/${var.git}-ssm-${random_string.identifier[0].result}" : ""
   deletion_window_in_days = var.kms_deletion_window_in_days
   account_actions = [
     for shared_account in var.shared_accounts : {
