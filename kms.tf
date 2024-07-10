31e0baf3a313fd6594e19b28df20ea42ac26a6ae
Phillip Langley <philliplangley@champmbplangley.lan>
2024-07-10 15:28:28 -0500

    test with enabled set false

--- kms.tf
+++ kms.tf
@@ -1,6 +1,6 @@
 module "kms" {
   count                   = var.enable_kms ? 1 : 0
-  source                  = "github.com/champ-oss/terraform-aws-kms.git?ref=v1.0.33-cb3be31"
+  source                  = "github.com/champ-oss/terraform-aws-kms.git?ref=v1.0.34-a5b529e"
   git                     = var.git
   name                    = "alias/${var.git}-ssm-${random_string.identifier[0].result}"
   deletion_window_in_days = var.kms_deletion_window_in_days
@@ -10,5 +10,6 @@ module "kms" {
       actions = ["kms:Decrypt"]
     }
   ]
-  tags = merge(local.tags, var.tags)
+  tags    = merge(local.tags, var.tags)
+  enabled = var.enable_kms
 }
