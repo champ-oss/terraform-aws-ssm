40df9a7a826fd3c28af11c7c824a6c0ef31d29dd
Phillip Langley <philliplangley@champmbplangley.lan>
2024-07-10 14:44:29 -0500

    enable toggle module

--- ram.tf
+++ ram.tf
@@ -1,5 +1,5 @@
 resource "aws_ram_resource_share" "this" {
-  count                     = length(var.shared_accounts) == 0 && !var.enabled ? 0 : 1
+  count                     = length(var.shared_accounts) == 0 && var.enabled != true ? 0 : 1
   name                      = "${var.git}-ssm-${random_string.identifier[0].result}"
   allow_external_principals = var.allow_external_principals
   permission_arns           = var.enable_ram_permission ? [aws_cloudformation_stack.ram_permission[0].outputs["RamPermissionArn"]] : []
@@ -13,7 +13,7 @@ resource "aws_ram_principal_association" "this" {
 }
 
 resource "aws_ram_resource_association" "this" {
-  count              = length(var.shared_accounts) == 0 && !var.enabled ? 0 : 1
+  count              = length(var.shared_accounts) == 0 && var.enabled != true ? 0 : 1
   resource_arn       = aws_ssm_parameter.this[0].arn
   resource_share_arn = aws_ram_resource_share.this[0].arn
 }
