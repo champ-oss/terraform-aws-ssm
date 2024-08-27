2edfe385a22e73cef1c1044de374e87f0c91bb2e
justin-champtitles <justin.reeves@champtitles.com>
2024-08-27 08:49:19 -0500

    use try function

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "arn" {
   description = "The ARN of the SSM parameter"
-  value       = var.enable_resources && var.enabled ? aws_ssm_parameter.this[0].arn : null
+  value = try(aws_ssm_parameter.this[0].arn, null)
 }
 
 output "name" {
   description = "The name of the SSM parameter"
-  value       = var.enable_resources && var.enabled ? aws_ssm_parameter.this[0].name : null
+  value = try(aws_ssm_parameter.this[0].name, null)
 }
 
 output "kms_key_arn" {
   description = "The KMS key ARN used to encrypt the SSM parameter"
-  value       = var.enable_kms ? module.kms[0].arn : null
+  value = try(module.kms[0].arn, null)
 }
 
 output "kms_key_id" {
   description = "The KMS key id used to encrypt the SSM parameter"
-  value       = var.enable_kms ? module.kms[0].key_id : null
+  value = try(module.kms[0].key_id, null)
 }
