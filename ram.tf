15226e13badf83eff451ccd729058162dfcd874d
Phillip Langley <philliplangley@champmbplangley.lan>
2025-08-12 10:12:38 -0500

    add enable flag on ram resource

--- ram.tf
+++ ram.tf
@@ -7,7 +7,7 @@ resource "aws_ram_resource_share" "this" {
 }
 
 resource "aws_ram_principal_association" "this" {
-  for_each           = toset(var.shared_accounts)
+  for_each           = var.enabled ? toset(var.shared_accounts) : toset([])
   principal          = each.value
   resource_share_arn = aws_ram_resource_share.this[0].arn
 }
